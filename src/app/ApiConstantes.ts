c523c03fc1a5c3be0229e20d3da98698ce882894
Nadine Anderle <nadianderle@gmail.com>
2018-04-16 08:43:52 -0300

    errado

--- src/app/ApiConstantes.ts
+++ src/app/ApiConstantes.ts
@@ -1,6 +1,6 @@
 export class ApiConsantes {
 
-    static readonly BASE_URL:string = "http://192.168.1.108:3000/api"
+    static readonly BASE_URL:string = "http://10.0.0.113:3000/api"
 
     static readonly Auth = {
         LOGIN: 'login'
